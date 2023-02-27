2eb79b65366e7320c7bb3fb4233f741af4f05d3a
Vallejos Jose Luis <joseluisvallejos1995@gmail.com>
2023-02-26 23:37:14 -0300

    feat: :construction: WIP

--- routers/index.js
+++ routers/index.js
@@ -2,7 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) =>{
-    res.end('hello word');
+    res.render('index', {
+        title: "My web store"
+    });
+});
+
+router.get('/products', (req, res, next) => {
+    res.render('products', {
+        title: "list of Products"
+    });
 });
 
 module.exports = router;
